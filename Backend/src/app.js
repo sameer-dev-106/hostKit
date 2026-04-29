8bfe8909a98e7cae34847b3ef8abf77c2582e70e
Sameer <sk6586418@gmail.com>
2026-04-29 23:20:41 +0530

    Add Passport configuration with Google and GitHub strategies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import { Strategy as GitHubStrategy } from "passport-github2";
+import { config } from "./config/config.js";
+
 // Import routes and middlewares
 import handleError from "./middlewares/error.middleware.js";
 import authRouter from "./routes/auth.routes.js";
@@ -15,6 +23,24 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
+
+// Passport configuration
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+}));
+
+// 
+passport.use(new GitHubStrategy({
+    clientID: config.GITHUB_CLIENT_ID,
+    clientSecret: config.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/api/auth/github/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+}));
 
 // Routes
 app.get("/", (_req, res) => {
