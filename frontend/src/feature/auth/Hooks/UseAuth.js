ff29ee84bb3dd7b2d7cb37b71c4f9b5b731daac6
Sameer <sk6586418@gmail.com>
2026-05-03 17:14:13 +0530

    Fix import case sensitivity for AuthContext in UseAuth and main files

--- frontend/src/feature/auth/Hooks/UseAuth.js
+++ frontend/src/feature/auth/Hooks/UseAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../auth.context";
+import AuthContext from "../Auth.context";
 import { registerApi, loginApi, getMeApi, } from "../services/auth.api";
 
 export const useAuth = () => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './feature/shared/global.scss'
-import { AuthProvider } from './feature/auth/auth.context.jsx'
+import { AuthProvider } from './feature/auth/Auth.context.jsx';
 
 createRoot(document.getElementById('root')).render(
 
